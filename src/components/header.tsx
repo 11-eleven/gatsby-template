bbd57984438a6e1cc6f388a44d29c97f90d13bfc
Stephanie Diep <stephanie.diep002@gmail.com>
2020-11-22 17:50:45 -0500

    Changed template to Typescript files

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,22 +3,17 @@ import styled from "styled-components"
 
 const Header = ({ siteTitle }) => (
   <Wrapper>
-    <ContentWrapper>
       <Logo>
         <a href="/">
           eleven
         </a>
       </Logo>
-    </ContentWrapper>
   </Wrapper>
 )
 
 export default Header
 
 const Wrapper = styled.div`
-`
-
-const ContentWrapper = styled.div`
   display: grid;
   padding: 40px;
 `
@@ -27,14 +22,6 @@ const Logo = styled.h1`
 
   a {
     color: black;
-    transition: 2s cubic-bezier(0.075, 0.82, 0.165, 1);
-
-    &:hover {
-      background: none;
-      border-bottom: 3px solid black;
-      padding: 0;
-      margin: 0 10px;
-    }
   }
 `
 
