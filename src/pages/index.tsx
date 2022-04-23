7d72cf169de84cc09f5067aa52c321fd78f9c398
stephdiep <stephanie.diep002@gmail.com>
2022-04-23 17:13:58 -0400

    Remove image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -16,10 +15,6 @@ const IndexPage = () => (
       <h1>Hi peeps</h1>
       <p>Welcome to your new Gatsby site. Now go build something great.</p>
     </TextWrapper>
-
-    <ImageWrapper>
-      <Image />
-    </ImageWrapper>
   </Layout>
 )
 
@@ -29,9 +24,4 @@ const TextWrapper = styled.div`
   display: grid;
   gap: 20px;
   padding: 0 0 20px;
-`
-
-const ImageWrapper = styled.div`
-  max-width: 300px;
-  margin-bottom: 30px;
 `
